3d3ea7be4b64b4ab99f1ad32308c79e469224557
Fabricio <fabricio_9327@hotmail.com>
2024-11-09 21:15:03 -0300

    create class JwtUtils

---
+++ src/main/java/com/sousa/demo_parking_api/jwt/JwtToken.java
@@ -0,0 +1,15 @@
+package com.sousa.demo_parking_api.jwt;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter@Setter
+public class JwtToken {
+
+    private String token;
+}
---
+++ src/main/java/com/sousa/demo_parking_api/jwt/JwtUtils.java
@@ -0,0 +1,86 @@
+package com.sousa.demo_parking_api.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "Bearer";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String SECRET_KEY = "123456789-123456789-123456789-12";
+    public static final long EXPIRE_DAYS = 0;
+    public static final long EXPIRE_HOURS = 0;
+    public static final long EXPIRE_MINUTES = 2;
+
+    public JwtUtils() {
+    }
+
+    private static Key generateKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate( Date start){
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static JwtToken createToken(String username, String role){
+        Date issueAt = new Date();
+        Date limit = toExpireDate(issueAt);
+
+        String token = Jwts.builder()
+                .subject(username)
+                .issuedAt(issueAt)
+                .expiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role", role)
+                .compact();
+
+        return new JwtToken(token);
+    }
+
+    private static  String refectorToken(String token){
+        if (token.contains(JWT_BEARER)){
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
+    }
+
+    private static Claims getClaimsFronToken(String token){
+        try {
+            return Jwts.parser().setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refectorToken(token)).getBody();
+
+        } catch (JwtException e){
+            log.error(String.format("token invalido", e.getMessage()));
+        }
+        return null;
+    }
+
+    public static String getUsernameFronToken(String token){
+        return getClaimsFronToken(token).getSubject();
+    }
+
+    public static boolean isTokenValid(String token){
+        try {
+            Jwts.parser().setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refectorToken(token));
+            return true;
+        } catch (JwtException e){
+            log.error(String.format("token invalido", e.getMessage()));
+        }
+        return false;
+    }
+}
